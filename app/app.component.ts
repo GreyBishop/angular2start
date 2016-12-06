7311474bb3fb5b1e56564b5fa082c41ee30886a0
Salgado <bsalgado@kpmg.com>
2016-12-06 15:18:25 -0500

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mtg-app',
+    template: `
+        <h1>Angular2: Starter Template</h1>
+    `
+})
+export class AppComponent { }
